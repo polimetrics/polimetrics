4aab9b91c8c5c7fa35a2081bfe3f246667fe74c5
bdcostin <bailey.costin@gmail.com>
2019-04-15 15:24:21 -0400

    updates Twitter stuff

--- core/twitter_search.py
+++ core/twitter_search.py
@@ -6,10 +6,10 @@ except ImportError:
 import tweepy
 
 # Variables that contains the user credentials to access Twitter API 
-ACCESS_TOKEN = '1116579167825305600-FICyHGcbHBv5weRMAArEjYEmQSkRVG'
-ACCESS_SECRET = 'sAbozMP6ochwoOhDKZ2M0Z3uh4LWWhdRMvjIGyGcFgzUj'
-CONSUMER_KEY = 'QWqHOOdvwYROiuAD7lDgZ45zU'
-CONSUMER_SECRET = 'eNj1D3bOYCdwVaIYJy2w5ybzv7dwkV12gxkTUoK1PJYV4nu3tj'
+ACCESS_TOKEN = ''
+ACCESS_SECRET = ''
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
