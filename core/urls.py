013741f52bd7675a0c6f46620cdf90ed3c52d87a
David Kirk <david.is.kirk@gmail.com>
2019-04-18 09:17:57 -0400

    created a url for candidate detail

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     # path('<slug:slug>/', views.candidates, name='candidate'),
-    path('/candidate/', views.candidates, name='candidates'),
-    path('/candidateDetail', views.candidateDetail, name='candidateDetail')
+    path('candidate/', views.candidates, name='candidates'),
+    path('candidateDetail/', views.candidateDetail, name='candidateDetail'),
     # path('<slug:slug>/', views.candidate, name='candidate'),
     path('', views.tags, name='tags'),
     path('about/', views.about, name='about')
