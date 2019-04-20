49cb2d410271f3fbf0f35b33d6e3be140cbbfb05
dmpeterson317 <dmpeterson317@gmail.com>
2019-04-19 21:04:29 -0400

    hf

--- core/management/commands/twitter_util.py
+++ core/management/commands/twitter_util.py
@@ -26,7 +26,7 @@ class Command(BaseCommand):
         ACCESS_SECRET = settings.ACCESS_SECRET
         CONSUMER_KEY = settings.CONSUMER_KEY
         CONSUMER_SECRET = settings.CONSUMER_SECRET
-
+        print(CONSUMER_KEY)
         auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
         auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
         api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
