56e8bcbedfad0c499ec0ccd42ebbd530857d4dc4
bdcostin <bailey.costin@gmail.com>
2019-04-21 14:45:18 -0400

    deletes commented code in view

--- core/views.py
+++ core/views.py
@@ -29,31 +29,6 @@ def index(request):
     script, div = components(plot)
     return render_to_response( "index.html", {'script' : script, 'div' : div} )
 
-# def index(request):
-#     candidates = ['Bernie Sanders', 'Joe Biden', 'Elizabeth Warren']
-#     counts = [.3, .4, .2]
-
-#     data = { 'candidates' : candidates }
-
-#     source = ColumnDataSource(data=dict(candidates=candidates, counts=counts))
-
-#     plot = figure(x_range=FactorRange(*candidates), plot_height=250, title="Average Sentiment to Date", toolbar_location=None, tools="")
-
-#     plot.vbar(x='candidates', top='counts', width=0.9, source=source, line_color="white", fill_color=factor_cmap('candidates', palette=Spectral6, factors=candidates))
-
-#     plot.y_range.start = 0
-#     plot.x_range.range_padding = 0.1
-#     plot.xaxis.major_label_orientation = 1
-#     plot.xgrid.grid_line_color = None
-
-#     script, div = components(plot)
-#     return render_to_response( "index.html", {'script' : script, 'div' : div} )
-
-
-# def candidates(request, slug):
-#     candidate = get_object_or_404(Candidate, slug=slug)
-#     return render(request, "candidates.html")
-
 def candidates(request):
     return render(request, "candidates.html")
 
