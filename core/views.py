54acd6f1f8b45207aaeee1968eee764f576b8626
dmpeterson317 <dmpeterson317@gmail.com>
2019-05-04 17:29:55 -0400

    add detail bar graph, make panel clickable, fix z-indexes

--- core/views.py
+++ core/views.py
@@ -5,9 +5,9 @@ from django.db.models import Min, Max
 from datetime import timedelta, datetime, timezone
 from bokeh.plotting import figure
 from bokeh.embed import components
-from bokeh.models import ColumnDataSource, HoverTool
-from bokeh.transform import cumsum
+from bokeh.models import ColumnDataSource, FactorRange, HoverTool
 from bokeh.models.widgets import Panel, Tabs
+from bokeh.transform import factor_cmap
 
 def index(request):
     color_list = []
@@ -53,10 +53,15 @@ def index(request):
     mode = 'vline'
     ) 
 
-    plot = figure(x_range=candidates_list, y_range=(-0.5, .5),
-                  x_axis_label='Candidates', y_axis_label='Sentiment',
-                  plot_height=600, plot_width=950, title="Average Sentiment Per Candidate for April 2019",
-                  tools=[hover, 'wheel_zoom', 'reset'], sizing_mode="scale_both")
+    plot = figure(x_range=candidates_list, 
+                            y_range=(-0.5, .5),
+                            x_axis_label='Candidates', 
+                            y_axis_label='Sentiment',
+                            plot_height=600, 
+                            plot_width=950, 
+                            title="Average Sentiment Per Candidate for April 2019",
+                            tools=[hover, 'wheel_zoom', 'reset'], 
+                            sizing_mode="scale_both")
     plot.title.text_font_size = "21px"
     plot.xaxis.axis_label_text_font_size = "19px"
     plot.yaxis.axis_label_text_font_size = "19px"
@@ -78,7 +83,7 @@ def candidate_detail(request, slug):
     daily_mean_sentiments = []
     daily_mean_sentiment_dates = []
     min_from_date_time_dict = CandidateMeanSentiment.objects.filter(candidate = candidate).aggregate(min_from_date_time = Min('from_date_time'))
-    
+
     # minimum from_date_time in CandidateMeanSentiment for candidate in database
     min_from_time = min_from_date_time_dict['min_from_date_time']
 
@@ -90,32 +95,36 @@ def candidate_detail(request, slug):
         daily_sentiment = CandidateMeanSentiment.objects.filter(
             candidate = candidate,
             from_date_time = min_from_time + timedelta(days=day),
-            to_date_time = min_from_time + timedelta(days=day+1)
-        )
+            to_date_time = min_from_time + timedelta(days=day+1))
+
         if daily_sentiment: # we should also order by created_at time in addition to to_date_time
             daily_mean_sentiment_dates.append(daily_sentiment[0].to_date_time)
             daily_mean_sentiments.append(daily_sentiment[0].mean_sentiment)
 
     agg_candidate_mean_sentiments = CandidateMeanSentiment.objects.filter(
         candidate = candidate,
-        from_date_time = min_from_time
-    )
-    print(daily_mean_sentiment_dates)
-    print(daily_mean_sentiments)
+        from_date_time = min_from_time)
+
     for mean_sentiment in agg_candidate_mean_sentiments:
         agg_mean_sentiment_dates.append(mean_sentiment.to_date_time)
         agg_mean_sentiments.append(mean_sentiment.mean_sentiment)
-    print("agg sentiments: ", agg_mean_sentiments)
-    print("agg Dates: ", agg_mean_sentiment_dates)
 
     detail_line_graph = figure(x_axis_label='Date of sentiment',
                             x_axis_type='datetime',
                             y_axis_label='Sentiment',
-                            plot_width=700,
-                            plot_height=350,
-                            toolbar_location=None,
+                            plot_width=800,
+                            plot_height=400,
+                            tools=['wheel_zoom', 'reset'],
                             y_range=(-0.5, 0.5), 
-                            sizing_mode="scale_both")
+                            sizing_mode="scale_both"
+                            )
+
+    detail_line_graph.line(daily_mean_sentiment_dates, 
+                            daily_mean_sentiments,  
+                            line_color='blue', 
+                            line_width=3, 
+                            alpha=.5,
+                            legend="Daily")
 
     detail_line_graph.line(agg_mean_sentiment_dates, 
                             agg_mean_sentiments,  
@@ -124,26 +133,69 @@ def candidate_detail(request, slug):
                             line_dash=[5,5],
                             alpha=.9,
                             legend="Aggregate")
-    detail_line_graph.line(daily_mean_sentiment_dates, 
-                            daily_mean_sentiments,  
-                            line_color='blue', 
-                            line_width=3, 
-                            alpha=.5,
-                            legend="Daily")
 
     # detail_line_graph.xaxis.major_label_orientation = pi/4
+
+    max_pos_engagement = agg_candidate_mean_sentiments.last().positive_engagement
+    max_neg_engagement = agg_candidate_mean_sentiments.last().negative_engagement
+
+    total_engagement = agg_candidate_mean_sentiments.last().total_engagement
+
+    max_pos_percent = (max_pos_engagement / total_engagement)*100
+    max_neg_percent = (max_neg_engagement / total_engagement)*100
+
+    print('neg: ', max_neg_engagement)
+    print('pos: ', max_pos_engagement)
+    print('total:', total_engagement)
+    print('neg percent:', max_neg_percent)
+
+    time_spans = ['daily', 'overall']
+    engagement_splits = ["Postive Percent", "Negative Percent"]
+
+    data = {
+        'daily/overall': time_spans,
+        'Positive Percent': [1, max_pos_percent],
+        'Negative Percent': [2, max_neg_percent]
+    }
+
+    palette = ["red", "green"]
+
+    # this creates [ ("daily", "Positive Percent"), ("daily", "Negative Percent"), ("overall", "Positive Percent"), ("overall", "Negative Percent") ]
+    x = [ (time_span, engagement_split) for time_span in time_spans for engagement_split in engagement_splits ]
+    counts = sum(zip(data['Positive Percent'], data['Negative Percent']), ()) # like an hstack
+
+    source = ColumnDataSource(data=dict(x=x, counts=counts))
+
+    # hover = HoverTool(
+    #     tooltips = [
+    #     (),
+    #     (),
+    # ],
+    # mode = 'vline'
+    # ) 
+
+    p = figure(x_range=FactorRange(*x), 
+                            plot_height=400,
+                            plot_width=800, 
+                            title="Daily/Overall Engagement Percentages",
+                            sizing_mode="scale_both",
+                            tools=['wheel_zoom', 'reset'])
+            
+    p.vbar(x='x', top='counts', width=0.9, source=source, line_color="white",
+                            fill_color=factor_cmap('x', palette=palette, factors=engagement_splits, start=1, end=2))
+    
+    p.y_range.start = 0
+    p.x_range.range_padding = 0.1
+    p.xaxis.major_label_orientation = 1
+    p.xgrid.grid_line_color = None
+
+
+
     tab1 = Panel(child=detail_line_graph, title="line")
 
-    # latest_engagement_pie_chart = figure(plot_height=350, 
-    #                         title="Number of Negative and Positive Activity Today", 
-    #                         toolbar_location=None,
-    #                         tools="hover",
-    #                         tooltips=""
-    #                         )
-    # latest_engagement_pie_chart.wedge(x=0, y=1, radius=0.4, start_angle=cumsum(
-    # tab2 = Panel(child=latest_engagement_pie_chart, title="pie")
+    tab2 = Panel(child=p, title="bar")
 
-    tabs = Tabs(tabs=[tab1])
+    tabs = Tabs(tabs=[tab1, tab2])
 
     script, div = components(tabs)
     context = {'script': script, 'div': div, 'candidate': candidate, 'candidates': candidates}
